aae2b1d6757211bb1673ebe1b079321a0f772241
unknown <fadlullahyazid@gmail.com>
2019-09-24 23:18:00 +0700

    First commit

---
+++ src/pbo11k/pkg10118907/latihan6/kambingstatic/Mamalia.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan6.kambingstatic;
+
+/**
+ *
+ * @author programmer
+ */
+public class Mamalia {
+    public static int jumlahKambing;
+}
---
+++ src/pbo11k/pkg10118907/latihan6/kambingstatic/PBO11K10118907Latihan6KambingStatic.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan6.kambingstatic;
+
+/**
+ *
+ * @author
+ * NAMA     : Yazid Fadlullah At-Taukily
+ * KELAS    : PBO11K
+ * NIM      : 10118907
+ * Deskripsi Program : Latihan mengakses variabel static di class lain
+ * 
+ */
+
+public class PBO11K10118907Latihan6KambingStatic {
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static final String NAMA_KAMBING = "MIDUN";
+    
+    public static void main(String[] args) {
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak "+Mamalia.jumlahKambing);
+    }
+    
+}
